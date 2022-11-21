944e6ed961842573c458448ce5082bef1a9a4871
John Tarleton Moore <tarletonmoore@gmail.com>
2022-11-21 13:10:12 -0500

    Done.

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,22 +7,31 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    toy = find_toy
+    toy.update!(toy_params)
+    render json: toy
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   def destroy
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.destroy
     head :no_content
   end
 
   private
+
+  def find_toy
+    Toy.find(params[:id])
+  end
   
   def toy_params
     params.permit(:name, :image, :likes)
